0bd02dc9d781e2724401fb59e9597b25a8c2da68
scorpioangry <scorpioangry@users.noreply.github.com>
2016-03-29 18:54:11 +0300

    Create awswindows.bat

---
+++ awswindows.bat
@@ -0,0 +1,16 @@
+set
+# Check current setting
+Get-ExecutionPolicy
+
+# Disable policy
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+# Choose [Y]es
+
+Get-WmiObject -Class Win32_Product | Select-Object -Property Name | `
+    Where {$_.Name -notlike "Microsoft*" -AND $_.Name -notlike "Windows*" -AND $_.Name -notlike "crystal*" -AND $_.Name -notlike "shared*" -AND $_.Name -notlike "tp.*"}  | sort-object -property Name > c:\1.txt 
+
+
+
+# Can be run immediately
+Set-ExecutionPolicy -ExecutionPolicy Default
+# [Y]es
